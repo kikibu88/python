8f0be1d55c491b46a469eeaa5cbfd3ad2cef095b
kikibu88 <kikibu88@gmail.com>
2018-08-02 20:51:34 +0900

    1장 끝

---
+++ type.py
@@ -0,0 +1,13 @@
+print (type(123))
+print (type(None))
+
+int_type=1
+float_type=1.1
+str_type="1"
+none_type=None
+
+print (type(int_type))
+print (type(float_type))
+print (type(str_type))
+print (type(none_type))
+
