6a1d5ec0401d2524e395d75e80e4ce2c619ddca2
Kibaek Kim <kibaek@peoplefund.co.kr>
2018-08-02 20:51:27 +0900

    마무리

---
+++ comment.py
@@ -0,0 +1,5 @@
+"""
+commnet
+"""
+
+# comtent a line
---
+++ type.py
@@ -0,0 +1,9 @@
+str_tp = "123"
+int_t = 123
+flt_t = 1.23
+non_t = None
+
+print(type(str_tp))
+print(type(int_t))
+print(type(flt_t))
+print(type(non_t))
